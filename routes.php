900d154fa019b108ab9d67e442ecfe6bc1c884c7
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-10-15 14:39:38 +0300

    update: update the routes

--- routes.php
+++ routes.php
@@ -2,7 +2,6 @@
 
 return [
     '/' => 'controllers/home.php',
-    '/index.php' => 'controllers/home.php',
     '/about' => 'controllers/about.php',
     '/blog' => 'controllers/blog.php',
     '/contact' => 'controllers/contact.php',
