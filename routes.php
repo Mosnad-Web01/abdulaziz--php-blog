02af54008ae10a432714b8964650321503bf25ae
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-10-15 15:16:11 +0300

    add

--- routes.php
+++ routes.php
@@ -4,5 +4,6 @@ return [
     '/' => 'controllers/home.php',
     '/about' => 'controllers/about.php',
     '/blog' => 'controllers/blog.php',
+    '/post' => 'controllers/post.php',
     '/contact' => 'controllers/contact.php',
 ];
--- views/includes/header.php
+++ views/includes/header.php
@@ -6,7 +6,7 @@
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">My Website</a>
+        <a class="navbar-brand" href="/">Ezz-Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,6 +33,12 @@
     <!-- Main Content Section -->
     <section class="container mt-5 text-center">
     </section>
+    <!-- Bootstrap CSS -->
+
+    <!-- Optional Bootstrap JS and jQuery (for any JavaScript features) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
